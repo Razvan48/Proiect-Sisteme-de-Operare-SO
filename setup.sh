318dcff73c8d0c3c1bcf645c4b883e93cc07aae1
Razvan48 <razvannicolae48@gmail.com>
2023-12-06 12:14:07 +0200

    setup 2

--- setup.sh
+++ setup.sh
@@ -2,11 +2,15 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 gcc "$SCRIPT_DIR/da.cpp" -o "$SCRIPT_DIR/da"
 
+echo "da.cpp has been compiled."
+
 echo $SCRIPT_DIR
 
-if [ ":$PATH:" != *":$SCRIPT_DIR:"* ]; then
+if echo $PATH | grep -q "$SCRIPT_DIR"; then
+	echo "Directory is already in the PATH."
+else
 	echo "export PATH=\$PATH:$SCRIPT_DIR" >> ~/.bashrc
 	export PATH=$PATH:$SCRIPT_DIR
+	echo "Directory added to PATH."
+	echo "Please reboot."
 fi
-
-echo $PATH
