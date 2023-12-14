1694ecb2653fa8af33d418f5c2f6a6038bca3439
TeodorL2 <151771015+TeodorL2@users.noreply.github.com>
2023-12-14 17:18:00 +0200

    communication da - daemon

--- da.cpp
+++ da.cpp
@@ -1,35 +1,143 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <semaphore.h>
 #include <string.h>
-#include <stdbool.h>
+#include <sys/wait.h>
 
-bool isOption(const char* user_option, const char* option1, const char* option2) {
-	return strcmp(user_option, option1) == 0 || strcmp(user_option, option2) == 0;
-}
 
-int main(int argc, char* argv[]) {
-    if (argc == 1) {
-        fprintf(stderr, "da: missing operand\n");
-        fprintf(stderr, "Try \'da --help\' for more information\n");
-        return 1;
-    }
+#define KILL 'k'
+#define ADD_MSG 'a'
+#define PRINT 'p'
 
-    if (isOption(argv[1], "-h", "--help")) {
-    	printf("Usage: da [OPTION]... [DIR]...\n"
-            "Analyze the space occupied by the directory at [DIR]\n"
-            "  -a, --add           analyze a new directory path for disk usage\n"
-            "  -p, --priority      set priority for the new analysis (works only with -a argument)\n"
-            "  -S, --suspend <id>  suspend task with <id>\n"
-            "  -R, --resume <id>   resume task with <id>\n"
-            "  -r, --remove <id>   remove the analysis with the given <id>\n"
-            "  -i, --info <id>     print status about the analysis with <id> (pending, progress, d\n"
-            "  -l, --list          list all analysis tasks, with their ID and the corresponding root p\n"
-            "  -p, --print <id>    print analysis report for those tasks that are \"done\"\n"
-            "  -t, --terminate     terminates daemon\n\n"
-        );
-        return 0;
-    }
 
-   	fprintf(stderr, "da: invalid option \'%s\'\n", argv[1]);
-   	fprintf(stderr, "Try \'da --help\' for more information\n");
+	char shm_name[] = "/shared_buffer";
+	char shm_semaphore_name[] = "/shared_semaphore";
+	size_t shm_size = 0;
+	size_t shm_semaphore_size = 0;
+	int shm_fd = 0;
+	int shm_semaphore_fd = 0;
+	char* shm_ptr = NULL;
+	void* shm_semaphore_ptr = NULL;
+	sem_t* sem = NULL;
+	char* option = NULL;
+	char* msg = NULL;
+
+void init_shm_buffer(); // initializare memorie partajata
+
+void init_shm_semaphore(); // initializare semafor partajat
+
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{		
+		printf("continutul help-ului.......\n");
+		return -1;
+	}
+	if(strcmp(argv[1], "-a") == 0 && argc < 3)
+	{		
+		printf("continutul help-ului.......\n");
+		return -1;
+	}
+
+	init_shm_buffer(); // obtin memoria partajata (si eventual pornesc daemonul)
+	init_shm_semaphore(); // obtin semaforul partajat
+	
+	if(strcmp(argv[1], "-k") == 0) 
+		*option = KILL;
+	else
+		if(strcmp(argv[1], "-a") == 0)
+			*option = ADD_MSG;
+		else
+			if(strcmp(argv[1], "-p") == 0)
+				*option = PRINT;
+			else
+			{
+				printf("continutul help-ului.......\n");
+				return -1;
+			} // setez optiunea (primul octet din memoria partajata)
+
+	if(*option == ADD_MSG)
+		strcpy(msg, argv[2]); // scriu mesajul (calea) in memoria partajata
+
+	if(*option == PRINT)
+		printf("da: %s\n", msg); // afisez din memoria partajata	
+
+	sem_post(sem); // daemonul era blocat in sem_wait(sem) (semaforul e zero) pana termin de scris in memoria partajata;
+					// il eliberez si imi termin executia
+
+
 	return 0;
 }
+
+
+void init_shm_buffer()
+{
+	shm_fd = shm_open(shm_name, O_RDWR, S_IRUSR | S_IWUSR); // obtin memoria partajata creata de daemon
+	if(errno == ENOENT) // daca primesc "no such file or directory" inseamna ca daemon-ul nu e pornit si il pornesc
+	{
+		pid_t pid = fork();
+		if(pid < 0)
+			exit(errno);
+
+		if(pid == 0) // proces copil
+		{
+			char* argv_c[30] = {"the_daemon", NULL};
+			execve("./the_daemon", argv_c, NULL); // rulez executabilul "the daemon"
+			perror("nu a pornit daemonul");
+		}
+		wait(NULL); // astapt sa se termine initializarea daemonului
+	// procesul parinte ("da") este radacina unei arborescente de forma: "da" -> "init_daemon" -> "daemon"
+	// "init_daemon" initializeaza obiectele de memorie partajata si moare => "daemon" pierde contactul cu terminalul;
+	// "da" stie ca initializarea s-a terminat si ca poate sa acceseze memoria partajata
+
+		shm_fd = shm_open(shm_name, O_RDWR, S_IRUSR | S_IWUSR);
+	}
+	if(shm_fd < 0)
+	{
+		perror(NULL);
+		exit(errno);
+	}
+
+	shm_size = getpagesize();
+	shm_ptr = (char*)mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if(shm_ptr == MAP_FAILED)
+	{
+		perror("map failed");
+		shm_unlink(shm_name);
+		exit(errno);
+	} // s-a mapat memoria partajata (un sir de caractere de 4096)
+	
+	option = shm_ptr; // primul caracter il tin special pentru transmiterea de optiuni
+	msg = shm_ptr + sizeof(char); // de aici incepe mesajul efectiv (calea catre directorul de analizat)
+}
+
+void init_shm_semaphore()
+{
+	shm_semaphore_fd = shm_open(shm_semaphore_name, O_RDWR, S_IRUSR | S_IWUSR);
+	if(shm_semaphore_fd < 0)
+	{
+		perror("no shared memory");
+		exit(errno);
+	}
+
+	shm_semaphore_size = getpagesize();
+	shm_semaphore_ptr = mmap(0, shm_semaphore_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_semaphore_fd, 0);
+	if(shm_semaphore_ptr == MAP_FAILED)
+	{
+		perror("map failed");
+		shm_unlink(shm_semaphore_name);
+		exit(errno);
+	} // am obtinut semaforul partajat cu daemonul
+	
+	sem = (sem_t*) shm_semaphore_ptr;
+}
+
+
---
+++ the_daemon.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <fstream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <semaphore.h>
+#include <chrono>
+#include <time.h>
+
+#define KILL 'k'
+#define ADD_MSG 'a'
+#define PRINT 'p'
+
+	pid_t pid;
+	size_t shm_size = 0;
+	size_t shm_semaphore_size = 0;
+	char shm_name[] = "/shared_buffer";
+	char shm_semaphore_name[] ="/shared_semaphore";
+	int shm_fd = 0;
+	int shm_semaphore_fd = 0;
+	char* shm_ptr = NULL;
+	void* shm_semaphore_ptr = NULL;
+	sem_t* sem = NULL;
+	bool isActive = true;
+	char* option = NULL;
+	char* msg = NULL;
+
+
+void init_shm_buffer();
+
+void init_shm_semaphore();
+
+void jobs();
+
+int main(int argc, char* argv[])
+{
+	init_shm_buffer(); // creez memoria partajata o singura data la pornirea daemonului
+	init_shm_semaphore(); // creez semaforul partajat o singura data la pornirea daemonului
+
+	pid = fork(); // creez un fiu, care va fi daemonul efectiv
+	if(pid < 0)
+		return errno; 
+	if(pid > 0) 
+	{
+		return 0;
+	} // parintele se termina; fiul (daemonul) a pierdut contactul cu terminalul; 
+		// procesul "da" care a pornit parintele se opreste din wait(NULL), stiind ca daemonul functioneaza
+
+	while(isActive)
+	{
+		sem_wait(sem); // semaforul a fost creat cu valoarea zero, deci astept sa il incrementeze "da" (<=> astept
+						// sa fie rulat ./da -option .... si sa imi trimita o comanda)
+
+		if(*option == KILL)
+		{
+			isActive = false;
+			continue; // 
+		}
+
+		jobs();    // "munca" efectiva a daemonului
+
+	}
+
+	shm_unlink(shm_name);
+	shm_unlink(shm_semaphore_name);
+	
+	return 0;
+}
+
+
+
+void init_shm_buffer()
+{
+	shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); // creez memoria partajata (functia se executa o singura data, la pornirea daemonului)
+	if(shm_fd < 0)
+	{
+		perror(NULL);
+		exit(errno);
+	}
+	shm_size = getpagesize();
+	if(ftruncate(shm_fd, shm_size) == -1)
+	{
+		perror(NULL);
+		shm_unlink(shm_name);
+		exit(errno);
+	}
+
+	shm_ptr = (char*) mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if(shm_ptr == MAP_FAILED)
+	{
+		perror(NULL);
+		shm_unlink(shm_name);
+		exit(errno);
+	}
+
+	option = shm_ptr; // optiunea - primul octet
+	msg = shm_ptr + sizeof(char); // mesajul (calea) restul
+	*option = '\0';
+	strcpy(msg, "");
+}
+
+void init_shm_semaphore()
+{
+	shm_semaphore_fd = shm_open(shm_semaphore_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); // creez semaforul partajat (functia se executa o singura data, la pornirea daemonului)
+	if(shm_semaphore_fd < 0)
+	{
+		perror(NULL);
+		exit(errno);
+	}
+	shm_semaphore_size = getpagesize();
+	if(ftruncate(shm_semaphore_fd, shm_size) == -1)
+	{
+		perror(NULL);
+		shm_unlink(shm_semaphore_name);
+		exit(errno);
+	}
+
+	shm_semaphore_ptr = mmap(0, shm_semaphore_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_semaphore_fd, 0);
+	if(shm_semaphore_ptr == MAP_FAILED)
+	{
+		perror(NULL);
+		shm_unlink(shm_semaphore_name);
+		exit(errno);
+	}
+
+	sem = (sem_t*) shm_semaphore_ptr;
+	sem_init(sem, 1, 0); // initializ semaforul cu zero, pentru ca daemonul sa se blocheze in asteptare pana la prima comanda de la "da"
+}
+
+void jobs()
+{
+	if(*option == ADD_MSG)
+		{
+			auto time_stamp = std::chrono::system_clock::now();
+			std::time_t current_time_epoch = std::chrono::system_clock::to_time_t(time_stamp);
+			char the_time[200] = "";
+			sprintf(the_time, " - timestamp  %s", std::ctime(&current_time_epoch));
+			strcat(msg, the_time); // doar adaug data si ora la mesajul primit de la "da"
+		}
+}
+
+
