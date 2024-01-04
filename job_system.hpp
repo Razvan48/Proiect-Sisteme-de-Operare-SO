e661d0504f1a5ebc612675ef35ee16e2583d1962
Razvan48 <razvannicolae48@gmail.com>
2024-01-04 23:44:47 +0200

    completed job_system.hpp

--- job_system.hpp
+++ job_system.hpp
@@ -5,12 +5,10 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <thread>
-#include <mutex>
-
-#include <memory>
-
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <pthread.h>
 
 //putem sa mutam implementarile ulterior intr-un cpp
 
@@ -18,9 +16,11 @@ int newId = 0; //variabila globala ce genereaza un nou id pentru fiecare task no
 
 //important: map-ul de jobs si de pathToId nu vor fi niciodata accesate de un job, ci doar de job system, deci nu trb mutex
 
+const int TIMP_ASTEPTARE_THREAD = 5; //cat asteapta un thread pana incearca sa-si reia activitatea dupa ce user-ul l-a suspendat
+
 struct Job;
 
-void threadWork(Job& job);
+void* threadWork(void* job);
 
 enum class JobStatus
 {
@@ -33,16 +33,18 @@ struct Job
 {
 	std::string path;
 	int priority;
-	std::thread thr;
-	std::mutex m;
+	pthread_t thr;
+	pthread_mutex_t m;
 	JobStatus status;
+	int nrBytesCrt;
+	int nrBytesTotal; //calculam procentul de finalizare ca 1.0 * nrBytesCrt / nrBytesTotal
 	
 	Job() = default;
 	
-	Job(const std::string& path, int priority):
-		path(path), priority(priority), status(JobStatus::Running), thr(threadWork, std::ref(*this)) //sper sa fie ok
+	Job(const std::string& path, int priority, JobStatus status):
+		path(path), priority(priority), status(status), nrBytesCrt(0), nrBytesTotal(0)
 	{
-		// TO DO: de initializat mutexul thread-ului mai intai (cred ca se intampla de la sine)
+	
 	}
 };
 
@@ -55,18 +57,28 @@ void addJob(const std::string& path, int priority = 1)
 	{
 		++newId;
 		pathToId[path] = newId;
-		//jobs[pathToId[path]] = {path, priority};
-		//jobs.emplace(std::make_pair((int)pathToId[path], Job(path, priority)));
-		//jobs[pathToId[path]](path, priority);
-		//jobs.insert(pathToId[path], Job(path, priority));
-		jobs[pathToId[path]].path = path; // TO DO: merge asa?
+		jobs[pathToId[path]].path = path;
 		jobs[pathToId[path]].priority = priority;
+		jobs[pathToId[path]].status = JobStatus::Running;
+		
+		if (pthread_mutex_init(&(jobs[pathToId[path]].m), NULL))
+		{
+			printf("Error: In addJob pthread_mutex_init failed\n");
+			perror(NULL);
+			return;
+		}
+		if (pthread_create(&(jobs[pathToId[path]].thr), NULL, threadWork, &(jobs[pathToId[path]])))
+		{
+			printf("Error: In addJob pthread_create failed\n");
+			perror(NULL);
+			return;
+		}
 	}
 	else //exista jobul (in acest caz doar dau update la prioritate)
 	{
-		jobs[pathToId[path]].m.lock();
+		pthread_mutex_lock(&(jobs[pathToId[path]].m));
 		jobs[pathToId[path]].priority = priority;
-		jobs[pathToId[path]].m.unlock();
+		pthread_mutex_unlock(&(jobs[pathToId[path]].m));
 	}
 }
 
@@ -77,11 +89,11 @@ void stopJob(int id) //seteaza flag-ul de status la un job ca Ending (thread-ul 
 	{
 		printf("Error: stopJob(%d): Invalid job id\n", id);
 		return;
-	}
-	
-	jobs.find(id)->second.m.lock();
+	}	
+
+	pthread_mutex_lock(&(jobs.find(id)->second.m));
 	jobs.find(id)->second.status = JobStatus::Ending;
-	jobs.find(id)->second.m.unlock();	
+	pthread_mutex_unlock(&(jobs.find(id)->second.m));
 }
 
 void pauseJob(int id) // seteaza flag-ul de status la un job ca Paused (thread-ul se va pune pe sleep cand va fi safe)
@@ -93,9 +105,9 @@ void pauseJob(int id) // seteaza flag-ul de status la un job ca Paused (thread-u
 		return;
 	}
 	
-	jobs.find(id)->second.m.lock();
+	pthread_mutex_lock(&(jobs.find(id)->second.m));
 	jobs.find(id)->second.status = JobStatus::Paused;
-	jobs.find(id)->second.m.unlock();
+	pthread_mutex_unlock(&(jobs.find(id)->second.m));
 }
 
 void unPauseJob(int id) // seteaza flag-ul de status la un job ca Running
@@ -107,9 +119,9 @@ void unPauseJob(int id) // seteaza flag-ul de status la un job ca Running
 		return;
 	}
 	
-	jobs.find(id)->second.m.lock();
+	pthread_mutex_lock(&(jobs.find(id)->second.m));
 	jobs.find(id)->second.status = JobStatus::Running;
-	jobs.find(id)->second.m.unlock();
+	pthread_mutex_unlock(&(jobs.find(id)->second.m));
 }
 
 void changePriority(int id, int priority) //reseteaza priority-ul unui job
@@ -121,9 +133,9 @@ void changePriority(int id, int priority) //reseteaza priority-ul unui job
 		return;
 	}
 	
-	jobs.find(id)->second.m.lock();
+	pthread_mutex_lock(&(jobs.find(id)->second.m));
 	jobs.find(id)->second.priority = priority;
-	jobs.find(id)->second.m.unlock();
+	pthread_mutex_unlock(&(jobs.find(id)->second.m));
 }
 
 void deleteJob(int id) //sterge job-ul cu id-ul dat
@@ -135,29 +147,166 @@ void deleteJob(int id) //sterge job-ul cu id-ul dat
 		return;
 	}
 	
-	jobs.find(id)->second.m.lock();
+	pthread_mutex_lock(&(jobs.find(id)->second.m));
 	std::string pathJob = jobs.find(id)->second.path;
-	pathToId.erase(pathJob);
-	jobs.find(id)->second.m.unlock();
+	pthread_mutex_unlock(&(jobs.find(id)->second.m));
 	stopJob(id);
+	if (pthread_join(jobs.find(id)->second.thr, NULL))
+	{
+		printf("Error: In deleteJob(%d) pthread_join failed\n", id);
+		perror(NULL);
+		return;
+	}
+	pthread_mutex_destroy(&(jobs.find(id)->second.m));
 	
-	//e thread-safe?
-	
+	pathToId.erase(pathJob);
 	jobs.erase(id);
 }
 
-void threadWork(Job& job) //functia rulata de fiecare thread in parte
+void processDirectory(const std::string& path, int& nrBytes, Job& job)
 {
-	// TO DO
-	
-	//mutex lock si unlock-uri folosind job.m
-	
-	/*
-	while (job.status == JobStatus::Running)
+	//cod de verificat status thread
+	//Paused
+	pthread_mutex_lock(&job.m);
+	while (job.status == JobStatus::Paused)
 	{
-		// TO DO
+		pthread_mutex_unlock(&job.m);
+		
+		///alt cod
+		sleep(TIMP_ASTEPTARE_THREAD);
+		
+		pthread_mutex_lock(&job.m);
 	}
-	*/
+	pthread_mutex_unlock(&job.m);
+	
+	//Ending
+	pthread_mutex_lock(&job.m);
+	if (job.status == JobStatus::Ending)
+	{
+		pthread_mutex_unlock(&job.m);
+		return;
+	}
+	pthread_mutex_unlock(&job.m);
+	//aici se incheie cod de verificare status thread
+
+	DIR *dir = opendir(path.c_str());
+
+	if (!dir)
+	{
+		printf("Error: In processDirectory(%s) opendir failed\n", path);
+		perror("Error opening directory");
+		return;
+	}
+	
+	struct dirent *entry;
+	struct stat fileStat;
+
+    	while ((entry = readdir(dir)) != NULL)
+    	{
+        	char filePath[1024];
+        	//snprintf(filePath, sizeof(filePath), "%s/%s", path, entry->d_name);
+
+        	if (stat(filePath, &fileStat) < 0)
+        	{
+            		perror("Error getting file status");
+            		continue;
+        	}
+
+		//este fisier sau director?
+        	if (S_ISDIR(fileStat.st_mode))
+        	{
+            		// Ignora directoarele "." (curent) si ".." (parintele)
+            		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            		{
+                		continue;
+            		}
+
+            		// Apelam recursiv
+            		processDirectory(filePath, nrBytes, job);
+        	}
+        	else //fisier
+        	{
+            		// Afisare fisier
+            		//printf("File: %s\n", filePath);
+
+            		// Adunam numarul de bytes
+            		
+            		pthread_mutex_lock(&job.m);
+            		nrBytes += fileStat.st_size;
+            		pthread_mutex_unlock(&job.m);
+        	}
+    	}
+    	
+    	closedir(dir);
+}
+
+void* threadWork(void* job) //functia rulata de fiecare thread in parte
+{
+	//cod de verificat status thread
+	//Paused
+	pthread_mutex_lock(&(((Job*)job)->m));
+	while (((Job*)job)->status == JobStatus::Paused)
+	{
+		pthread_mutex_unlock(&(((Job*)job)->m));
+		
+		///alt cod
+		sleep(TIMP_ASTEPTARE_THREAD);
+		
+		pthread_mutex_lock(&(((Job*)job)->m));
+	}
+	pthread_mutex_unlock(&(((Job*)job)->m));
+	
+	//Ending
+	pthread_mutex_lock(&(((Job*)job)->m));
+	if (((Job*)job)->status == JobStatus::Ending)
+	{
+		pthread_mutex_unlock(&(((Job*)job)->m));
+		return NULL;
+	}
+	pthread_mutex_unlock(&(((Job*)job)->m));
+	//aici se incheie cod de verificare status thread
+	
+	
+	///cod propriu-zis
+
+	pthread_mutex_lock(&(((Job*)job)->m));
+	std::string path = ((Job*)job)->path;
+	pthread_mutex_unlock(&(((Job*)job)->m));
+	
+	processDirectory(path, ((Job*)job)->nrBytesTotal,*((Job*)job)); //mai intai vedem cati bytes sunt in total, apoi mai parcurgem inca o data
+	//facem 2 parcurgeri in loc de una doar ca sa putem afisa o evolutie liniara procentului de finalizare atunci cand il cere userul
+	
+	
+	//alt cod pt a verifica daca user-ul vrea sa inchida thread-ul
+	
+	//Paused
+	pthread_mutex_lock(&(((Job*)job)->m));
+	while (((Job*)job)->status == JobStatus::Paused)
+	{
+		pthread_mutex_unlock(&(((Job*)job)->m));
+		
+		///alt cod
+		sleep(TIMP_ASTEPTARE_THREAD);
+		
+		pthread_mutex_lock(&(((Job*)job)->m));
+	}
+	pthread_mutex_unlock(&(((Job*)job)->m));
+	
+	//Ending
+	pthread_mutex_lock(&(((Job*)job)->m));
+	if (((Job*)job)->status == JobStatus::Ending)
+	{
+		pthread_mutex_unlock(&(((Job*)job)->m));
+		return NULL;
+	}
+	pthread_mutex_unlock(&(((Job*)job)->m));
+	
+	//cod propriu-zis
+	
+	
+	processDirectory(path, ((Job*)job)->nrBytesCrt, (*((Job*)job)));
+	
+	return NULL; //
 }
 
 
