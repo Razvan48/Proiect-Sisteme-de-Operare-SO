92a5a6a8c5116539fbe246a4b022344f5a74ea21
TeodorL2 <151771015+TeodorL2@users.noreply.github.com>
2024-01-05 19:03:57 +0200

    two buffers (input and output) + better semaphores

--- da.cpp
+++ da.cpp
@@ -22,17 +22,21 @@
 #define PRINT 'p'
 
 
-	char shm_name[] = "/shared_buffer";
-	char shm_semaphore_name[] = "/shared_semaphore";
-	size_t shm_size = 0;
-	size_t shm_semaphore_size = 0;
-	int shm_fd = 0;
-	int shm_semaphore_fd = 0;
-	char* shm_ptr = NULL;
-	void* shm_semaphore_ptr = NULL;
+	char shm_input_name[] = "/shared_buffer_input";
+	char shm_output_name[] = "/shared_buffer_output";
+	char semaphore_1_name[] = "/shared_semaphore_1";
+	char semaphore_2_name[] = "/shared_semaphore_2";
+
+	size_t shm_size_input = 0;
+	size_t shm_size_output = 0;
+	int shm_input_fd = 0;
+	int shm_output_fd = 0;
+	char* shared_buffer_input = NULL;
+	char* shared_buffer_output = NULL;
 	sem_t* sem_1 = NULL;
 	sem_t* sem_2 = NULL;
 	char* option = NULL;
+	char* priority = NULL;
 	char* msg = NULL;
 
 void init_shm_buffer(); // initializare memorie partajata
@@ -129,15 +133,25 @@ int main(int argc, char* argv[])
 	sem_wait(sem_2); // astept sa termine daemonul de lucrat cu memoria partajata
 	*option = opt;
 
-	if(*option == ADD_MSG)
-		strcpy(msg, argv[2]); // scriu mesajul (calea) in memoria partajata
-
 	if(*option == PRINT)
-		printf("da: %s\n", msg); // afisez din memoria partajata	
+	{
+		sem_post(sem_1);
+		sem_wait(sem_2);
+		printf("da: %s\n", shared_buffer_output); // afisez din memoria partajata	
+	}
+	else
+	{
+		if(*option == ADD_MSG)
+		strcpy(msg, argv[2]); // scriu mesajul (calea) in memoria partajata
 
 	sem_post(sem_1); // daemonul era blocat in sem_wait(sem) (semaforul e zero) pana termin de scris in memoria partajata;
 					// il eliberez si imi termin executia
+	}
 
+	sem_close(sem_1);
+	sem_close(sem_2);
+	munmap(shared_buffer_input, shm_size_input);
+	munmap(shared_buffer_output, shm_size_output);
 
 	return 0;
 }
@@ -145,7 +159,8 @@ int main(int argc, char* argv[])
 
 void init_shm_buffer()
 {
-	shm_fd = shm_open(shm_name, O_RDWR, S_IRUSR | S_IWUSR); // obtin memoria partajata creata de daemon
+	// input buffer
+	shm_input_fd = shm_open(shm_input_name, O_RDWR, S_IRUSR|S_IWUSR); // obtin memoria partajata creata de daemon
 	if(errno == ENOENT) // daca primesc "no such file or directory" inseamna ca daemon-ul nu e pornit si il pornesc
 	{
 		pid_t pid = fork();
@@ -163,45 +178,63 @@ void init_shm_buffer()
 	// "init_daemon" initializeaza obiectele de memorie partajata si moare => "daemon" pierde contactul cu terminalul;
 	// "da" stie ca initializarea s-a terminat si ca poate sa acceseze memoria partajata
 
-		shm_fd = shm_open(shm_name, O_RDWR, S_IRUSR | S_IWUSR);
+		shm_input_fd = shm_open(shm_input_name, O_RDWR, S_IRUSR|S_IWUSR);
 	}
-	if(shm_fd < 0)
+	if(shm_input_fd < 0)
 	{
 		perror(NULL);
 		exit(errno);
 	}
 
-	shm_size = getpagesize();
-	shm_ptr = (char*)mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-	if(shm_ptr == MAP_FAILED)
+	shm_size_input = getpagesize();
+	shared_buffer_input = (char*)mmap(0, shm_size_input, PROT_READ | PROT_WRITE, MAP_SHARED, shm_input_fd, 0);
+	if(shared_buffer_input == MAP_FAILED)
 	{
 		perror("map failed");
-		shm_unlink(shm_name);
+		shm_unlink(shm_input_name);
 		exit(errno);
 	} // s-a mapat memoria partajata (un sir de caractere de 4096)
 	
-	option = shm_ptr; // primul caracter il tin special pentru transmiterea de optiuni
-	msg = shm_ptr + sizeof(char); // de aici incepe mesajul efectiv (calea catre directorul de analizat)
+	option = shared_buffer_input; // optiune
+	priority = shared_buffer_input + sizeof(char); // prioritate, daca este cazul
+	msg = shared_buffer_input + 2*sizeof(char); // de aici incepe mesajul efectiv (calea catre directorul de analizat)
+
+
+	// output buffer
+	shm_output_fd = shm_open(shm_output_name, O_RDWR, S_IRUSR|S_IWUSR); 
+	if(shm_output_fd < 0)
+	{
+		perror(NULL);
+		shm_unlink(shm_input_name); // daca am eroare abia aici, inseamna ca obiectele pentru input s-au creat, deci le sterg
+		munmap(shared_buffer_input, shm_size_input);
+		exit(errno);
+	}
+
+	shm_size_output = getpagesize();
+	shared_buffer_output = (char*)mmap(0, shm_size_output, PROT_READ | PROT_WRITE, MAP_SHARED, shm_output_fd, 0);
+	if(shared_buffer_output == MAP_FAILED)
+	{
+		perror("map failed");
+		shm_unlink(shm_input_name);
+		shm_unlink(shm_output_name);
+		munmap(shared_buffer_input, shm_size_input);
+		exit(errno);
+	}
 }
 
 void init_shm_semaphore()
 {
-	shm_semaphore_fd = shm_open(shm_semaphore_name, O_RDWR, S_IRUSR | S_IWUSR);
-	if(shm_semaphore_fd < 0)
+	sem_1 = sem_open(semaphore_1_name, O_RDWR);
+	if(sem_1 == SEM_FAILED)
 	{
-		perror("no shared memory");
+		perror(NULL);
 		exit(errno);
 	}
 
-	shm_semaphore_size = getpagesize();
-	shm_semaphore_ptr = mmap(0, shm_semaphore_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_semaphore_fd, 0);
-	if(shm_semaphore_ptr == MAP_FAILED)
+	sem_2 = sem_open(semaphore_2_name, O_RDWR);
+	if(sem_2 == SEM_FAILED)
 	{
-		perror("map failed");
-		shm_unlink(shm_semaphore_name);
+		perror(NULL);
 		exit(errno);
-	} // am obtinut semaforul partajat cu daemonul
-	
-	sem_1 = (sem_t*) shm_semaphore_ptr;
-	sem_2 = (sem_t*) shm_semaphore_ptr + sizeof(sem_t);
+	}
 }
