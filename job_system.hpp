f92e89a0de5022c6ef469c779ce8e3fb89d73c4f
sebimih13 <sebimih@gmail.com>
2024-01-16 17:55:14 +0200

    bug

--- job_system.hpp
+++ job_system.hpp
@@ -560,13 +560,13 @@ std::string printAnalysisReportDirectory(const std::string& root, const std::str
 			continue;
 		}
 
-		if ((direntp->d_type == DT_LNK) // symbolic link
+		if (direntp->d_type == DT_LNK) // symbolic link
 		{
 			std::string filePath = path + "/" + direntp->d_name;
 			
 			struct stat st;
 			stat(filePath.c_str(), &st);
-			nrBytes += fileStat.st_size;
+			nrBytes += st.st_size;
 		}
 		else if (direntp->d_type == DT_REG)	// regular file
 		{
